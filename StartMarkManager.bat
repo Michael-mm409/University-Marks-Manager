f1e058b69bec9adaad5bdfa56d1f9fd37fbdbfc4
Michael-mm409 <mcmillanbmac@gmail.com>
2025-07-03 08:59:42 +1000

    feat: Refactor analytics display imports and remove legacy wrapper for improved modular architecture

--- StartMarkManager.bat
+++ StartMarkManager.bat
@@ -1,4 +1,4 @@
 REM filepath: c:\Users\micbm\OneDrive - University of Wollongong\UOW\Bachelor of Computer Science\Assignment Marks\University-Marks-Manager\StartMarkManager.bat
-SET PYTHONPATH=%CD%
+
 CALL conda activate UniversityMarksManager
 CALL streamlit run src/main.py --server.port 8501
