d941361393d36e771afeb7ecfdcab427eee0880e
Michael McMillan <mcmillanbmac@gmail.com>
2026-09-04 17:27:58 +1000

    security: remove query_db.py and ignore scratch scripts

--- query_db.py
+++
@@ -1,6 +0,0 @@
-import psycopg2
-conn = psycopg2.connect(host="192.168.8.8", user="Michael", password="Mickyb26*", database="marks-manager-db")
-cur = conn.cursor()
-cur.execute("SELECT s.subject_name, a.assessment, a.weighted_mark, a.unweighted_mark, a.mark_weight FROM assignments a JOIN subjects s ON a.subject_id = s.id WHERE s.subject_name LIKE '%Statistics%';")
-for row in cur.fetchall():
-    print(row)
